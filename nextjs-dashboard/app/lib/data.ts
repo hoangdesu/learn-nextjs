b977160f672c10868f6bc00e7837d1ef5071bd2a
hoangdesu <hoangdesu@gmail.com>
2024-05-05 14:33:49 +0700

    chap8: dynamic rendering, noStore for data caching, simulating slow data fetch and page block

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -9,22 +9,24 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
+  noStore();
 
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     // calling sql queries directly in server components
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -34,6 +36,7 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  noStore();
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -47,7 +50,7 @@ export async function fetchLatestInvoices() {
       amount: formatCurrency(invoice.amount),
     }));
     // console.log('latestInvoices', latestInvoices);
-    
+
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
@@ -56,6 +59,7 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  noStore();
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -69,7 +73,7 @@ export async function fetchCardData() {
 
     // The returned promise fulfills when all of the input's promises fulfill
     // Using Promise.all() to avoid 'request waterfall' -> fetch all in parallel
-    // do not use await above to extract the resolve data. 
+    // do not use await above to extract the resolve data.
     // Instead, pass in all the promises and await for them all at once
     const data = await Promise.all([
       invoiceCountPromise,
@@ -101,6 +105,7 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
+  noStore();
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
@@ -133,6 +138,7 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
+  noStore();
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -154,6 +160,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  noStore();
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -179,6 +186,7 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
+  noStore();
   try {
     const data = await sql<CustomerField>`
       SELECT
@@ -197,6 +205,7 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
+  noStore();
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
@@ -230,6 +239,7 @@ export async function fetchFilteredCustomers(query: string) {
 }
 
 export async function getUser(email: string) {
+  noStore();
   try {
     const user = await sql`SELECT * FROM users WHERE email=${email}`;
     return user.rows[0] as User;
--- nextjs-dashboard/app/youcantseeme/page.tsx
+++ nextjs-dashboard/app/youcantseeme/page.tsx
@@ -1,5 +1,9 @@
 // apparently function name doesn't quite matter for page component? :/
+// 'use client';
 export default function YouCantSeeMe() {
+  console.log("you can't see me page loaded");
+  // this is a server component -> not gonna see log on browser, but in terminal
+  // if using 'use client', logs can be seen from both client and server
   return (
     <div>
       <h1>You can&apos;t see me 👀</h1>
