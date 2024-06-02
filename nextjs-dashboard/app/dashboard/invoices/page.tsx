024993037b21b9e85ead57c8a52f4f14adc5a2da
hoangdesu <hoangdesu@gmail.com>
2024-06-03 02:40:06 +0700

    chap11: updating Table component with page's searchParams

--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -6,7 +6,21 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 
-export default function Invoices() {
+interface PageProps {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}
+
+export default function Invoices({ searchParams }: PageProps) {
+  // Extract the searchParams from page prop and pass down to Table component
+  // Give default values if searchParams are missing
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  console.log(query + ':' + currentPage);
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -16,9 +30,9 @@ export default function Invoices() {
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
-      </Suspense> */}
+      </Suspense>
       <div className="mt-5 flex w-full justify-center">
         {/* <Pagination totalPages={totalPages} /> */}
       </div>
