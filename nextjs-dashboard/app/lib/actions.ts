41d76d9d06bf8cf9b3736b00d8fb62b763dbbf66
hoangdesu <hoangdesu@gmail.com>
2024-07-21 22:49:26 +0700

    chap12: create new invoice and insert into db

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -1,11 +1,50 @@
 'use server';
 
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  // const rawFormData = {
+  //   customerId: formData.get('customerId'),
+  //   amount: formData.get('amount'),
+  //   status: formData.get('status'),
+  // };
+  // Test it out:
+  // console.log(rawFormData);
+
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
-  // Test it out:
-  console.log(rawFormData);
+  });
+
+  // good practice to store monetary values in cents in database to eliminate JavaScript floating-point errors and ensure greater accuracy.
+  const amountInCents = amount * 100;
+
+  const date = new Date().toISOString().split('T')[0]; // get current date in YYYY-MM-DD
+
+  console.log('> creating invoice: ', customerId, amount, status, amountInCents, date);
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  // clear this cache and trigger a new request to the server (fresh data)
+  revalidatePath('/dashboard/invoices');
+
+  // redirect user back to previous page
+  redirect('/dashboard/invoices');
 }
