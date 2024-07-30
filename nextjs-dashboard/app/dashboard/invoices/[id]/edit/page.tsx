35e602dd79fea04425e8b13e49a1ac733f31b116
hoangdesu <hoangdesu@gmail.com>
2024-07-31 00:38:34 +0700

    chap13: handling not-found error

--- nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,8 +1,9 @@
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import Form from '@/app/ui/invoices/edit-form';
+import { notFound } from 'next/navigation';
 
-export default async function EditInvoiceIdPage({
+export default async function EditInvoicePage({
   params,
 }: {
   params: { id: string };
@@ -14,8 +15,15 @@ export default async function EditInvoiceIdPage({
     fetchCustomers(),
   ]);
 
-//   console.log(invoice, customers);
-  
+  // - Throws error when an invoice is not found
+  // - Show UI from file not-found.tsx
+  // - Handle a more specific error
+  if (!invoice) {
+    notFound();
+  }
+
+  // console.log(invoice, customers);
+
   return (
     <main>
       <Breadcrumbs
