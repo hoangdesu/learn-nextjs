70558b16c5134978dfaaaf54e2b5f31208ecb912
hoangdesu <hoangdesu@gmail.com>
2025-03-15 20:00:13 +0700

    fix: vercel next auth url variable that auto redirects to wrong url after logging in

--- nextjs14-acme-dashboard-vercel-tutorial/app/ui/dashboard/sidenav.tsx
+++ nextjs14-acme-dashboard-vercel-tutorial/app/ui/dashboard/sidenav.tsx
@@ -21,7 +21,7 @@ export default function SideNav() {
             await signOut();
           }}
         >
-          <button className="items-cent er flex h-[48px] w-full grow justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <button className="items-center flex h-[48px] w-full grow justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
